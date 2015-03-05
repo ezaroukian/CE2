b647f5a672b0b98d72f73b01f0b5d5111d35783d
ezaroukian <egz@jhu.edu>
2015-03-05 11:51:14 +0000

    Update genData.js

--- js_includes/genData.js
+++ js_includes/genData.js
@@ -199,7 +199,7 @@ function ibexItem(onto,CErule){
 	var quest = image+"<br>"+CErule.rule;
 	var ans = ["Yes","No"];	
 	var cor = 1;
-	switch(CErule.type[0:5]){
+	switch(CErule.type.split("_")[0]){
 		case "11n13":
 			if (onto.type == "11" || onto.type == "31"){cor=0;}
 			break;
